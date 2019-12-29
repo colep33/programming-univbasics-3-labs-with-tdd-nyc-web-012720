6acb03b47cb9d3d015abb1c5ad36dbf5521c4b33
Cole Arthur Parham <coleparham@gmail.com>
2019-12-29 22:55:01 +0000

    Automatically backed up by Learn

--- new.rb
+++ new.rb
@@ -2,12 +2,12 @@ sauced = true
 soarin = true
 
 def wyd(sauced,soarin)
-  if sauced = true
+  if sauced == true
     puts "saucy fool"
   else
     puts "square boy"
   end
-  if soarin = true
+  if soarin == true
     puts "big dope"
   else
     puts "square boy"
